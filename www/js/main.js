c78bb08f0e6e03ff480d34d0f8028cf92d85dfd9
Ondrej Janata <o.janata@gmail.com>
2014-01-13 10:25:27 +0100

    Pridana obsluha chyboveho stavu ajaxoveho pozadavku

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,22 @@
-$(function(){
-
+jQuery(document).ready(function(){
+    /* Init NETTE AJAX */
+    $.nette.init();
+    $.nette.ext("sidebar", {
+        before: function(){
+            jQuery("#status-modal").show();
+        },
+        complete: function(data){
+            jQuery("#status-modal").fadeOut(500);
+        },
+        success: function(data){
+            var product_id = "#sidebar-product-id-" + data.product_id.toString();
+            jQuery(product_id).fadeTo('fast', 0.3).fadeTo(1000, 1.0); // zvyrazneni zmeneneho produktu
+        },
+        error: function(request, textStatus, errorThrown){
+            // informovani uzivatele, ze doslo k chybe
+            jQuery("#snippet--flash").html("<span class=\"alert alert-error\"><i class=\"icon-warning-sign\"></i>" +
+                "Při požadavku došlo k neočekávané chybě! Zkuste prosím akci zopakovat. " +
+                "V případě přetrvání problémů kontaktujte administrátora.</span>");
+        }
+    });
 });
